f742fb02c2456f12a83384e88f9400807255960e
Yeison Garcia <garciamonsefu@gmail.com>
2023-06-05 02:44:40 -0500

    Follow button styles

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,22 @@ import { FollowCard } from './FollowCard';
 
 
 export function App() {
-    return (
-        <section className='component-followCard'>
-            <b className='followCard-title'>A quién seguir</b>
-            <FollowCard userName='S4vitar'>S4vitar</FollowCard>
-            <FollowCard userName='BillGates'>Bill Gates</FollowCard>
-            <FollowCard userName='midudev'>Miguel Ángel Durán</FollowCard>
-            <button className='followCard-button-more'>Show more</button>
-        </section>
-    );
+  return (
+    <section className='component-followCard'>
+      <b className='followCard-title'>A quién seguir</b>
+      <FollowCard userName='S4vitar' isFollowing>
+        S4vitar Mendoza Hernandez
+      </FollowCard>
+
+      <FollowCard userName='BillGates' isFollowing={false}>
+        Bill Gates
+      </FollowCard>
+
+      <FollowCard userName='midudev' isFollowing={false}>
+        Miguel Ángel Durán
+      </FollowCard>
+
+      <button className='followCard-button-more'>Show more</button>
+    </section>
+  );
 }
--- src/FollowCard.jsx
+++ src/FollowCard.jsx
@@ -1,14 +1,25 @@
-export function FollowCard({ children, userName }) {
+export function FollowCard({ children, userName, isFollowing }) {
+
+    const textButton = isFollowing ? 'Siguiendo' : 'Seguir';
+    const classButton = isFollowing ? 'isFollowing' : '';
+
     return (
         <article className="followCard">
             <img className="avatar" alt="Avatar"
                 src={`https://unavatar.io/${userName}`} />
             <div className="followCard-content">
                 <div className="followCard-info">
-                    <strong>{children}</strong>
-                    <span className='followCard-infoUserName'>@{userName}</span>
+                    <strong className={`followCard-user ${classButton}`}>
+                        {children}
+                    </strong>
+                    <span className='followCard-info-userName'>
+                        @{userName}
+                    </span>
                 </div>
-                <button className="followCard-buttonFollow">Seguir</button>
+                <button className={`followCard-button ${classButton}`}>
+                    <span className="button-text">{textButton}</span>
+                    <span className="button-text-stopFollow">Dejar de seguir</span>
+                </button>
             </div>
         </article>
     );
